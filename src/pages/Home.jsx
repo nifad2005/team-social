050bf1ae6134fe32bf6d68ccac9ac7ac0fd3801f
Md Nifad Uzzaman <nifaduzzaman2005@gmail.com>
2025-03-05 01:11:14 +0600

    removed murad

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,6 @@ function Home() {
     <div>
       <h1>Welcome to Team Social</h1>
       
-      <h2 className='text-6xl font-bold text-rose-600'>this is murad</h2>
 
       <p>Connect with your team and collaborate effectively.</p>
       <button>Get Started</button>
