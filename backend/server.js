abcf3f7c94de45db9ba913b4cceb9d26f1a7445d
Md Nifad Uzzaman <nifaduzzaman2005@gmail.com>
2025-03-05 01:54:52 +0600

    Recreated the backend folder

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware setup
-app.use(cors());
+// app.use(cors());
 app.use(express.json()); // For parsing application/json
 
 // Basic route
