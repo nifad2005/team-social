dd1510396ff0f823dfb9432098fbc641e2ea03fc
murad <vvanhellsing30@gmail.com>
2025-03-05 00:31:33 +0600

    murad

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ function Home() {
   return (
     <div>
       <h1>Welcome to Team Social</h1>
+      <h2 className='text-6xl font-bold text-rose-600'>this is murad</h2>
       <p>Connect with your team and collaborate effectively.</p>
       <button>Get Started</button>
     </div>
