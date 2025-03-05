701db13481dc88a5d363c86a2712838ffc28cb58
murad <vvanhellsing30@gmail.com>
2025-03-05 14:14:38 +0600

    romove all

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,9 @@
-import React from 'react'
-import Post from '../components/Post'
+
 
 function Home() {
   return (
     <div>
-      <Post></Post>
-      <h1>Welcome to Team Social</h1>
-      <h2 className='text-6xl font-bold text-rose-600'>this is murad</h2>
-      <p>Connect with your team and collaborate effectively.</p>
-      <button>Get Started</button>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, accusantium excepturi corrupti voluptatem cupiditate, natus ad sit amet praesentium vero quod eveniet dolore! Omnis fugiat ratione nesciunt dolores voluptatum doloribus.</p>
+      
     </div>
   )
 }
