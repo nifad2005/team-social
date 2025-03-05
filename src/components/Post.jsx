a409c116680ff2460a9d7d2a618b524b04ef082f
murad <vvanhellsing30@gmail.com>
2025-03-05 14:49:52 +0600

    romove lorem div

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -45,12 +45,6 @@ const Post = () => {
                     </div>
                 }
             </div>
-            <div>
-                <h1>new change</h1>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, dicta.</p>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, dicta.</p>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, dicta.</p>
-            </div>
         </div>
     );
 };
