08d137dc979118c5ee2e86b278c44cf04c1073de
murad <vvanhellsing30@gmail.com>
2025-03-05 12:57:11 +0600

    post section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Post from '../components/Post'
 
 function Home() {
   return (
     <div>
+      <Post></Post>
       <h1>Welcome to Team Social</h1>
       <h2 className='text-6xl font-bold text-rose-600'>this is murad</h2>
       <p>Connect with your team and collaborate effectively.</p>
