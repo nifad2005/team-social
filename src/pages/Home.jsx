40057164d7afdbb0992c6245c1159909c5a56d7d
Md Nifad Uzzaman <nifaduzzaman2005@gmail.com>
2025-03-05 14:17:54 +0600

    Resolving

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,9 @@
-import React from 'react'
+
 
 function Home() {
   return (
     <div>
-      <h1>Welcome to Team Social</h1>
-      <h2 className='text-6xl font-bold text-rose-600'>this is murad</h2>
-      <p>Connect with your team and collaborate effectively.</p>
-      <button>Get Started</button>
+  
     </div>
   )
 }
