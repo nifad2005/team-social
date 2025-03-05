f782b4a4e97a26bff969aba12ec7a02f11374064
murad <vvanhellsing30@gmail.com>
2025-03-05 14:11:30 +0600

    adfa

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,6 +9,7 @@ function Home() {
       <h2 className='text-6xl font-bold text-rose-600'>this is murad</h2>
       <p>Connect with your team and collaborate effectively.</p>
       <button>Get Started</button>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, accusantium excepturi corrupti voluptatem cupiditate, natus ad sit amet praesentium vero quod eveniet dolore! Omnis fugiat ratione nesciunt dolores voluptatum doloribus.</p>
     </div>
   )
 }
