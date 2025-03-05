5d6cb690415006748c78c8b6de9b58687af4ec68
Md Nifad Uzzaman <nifaduzzaman2005@gmail.com>
2025-03-05 13:47:44 +0600

    Hello

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,7 @@ import React from 'react'
 function Home() {
   return (
     <div>
-      <h1>Welcome to Team Social</h1>
-      
-
-      <p>Connect with your team and collaborate effectively.</p>
-      <button>Get Started</button>
-    <h2>hello saisob</h2>
+        Don't make changes here.
     </div>
   )
 }
